c11b759b2998325a9684d0ae78bbe49f39af9388
David Eisenstein <davidseisenstein@gmail.com>
2020-09-21 15:47:52 -0500

    Updated html to take a date input, and built new function. Date strings not matching

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,16 +6,46 @@ tableData = data
 
 tBody = d3.select("tbody")
 
-// Loop through each object in the tableData array using arrow notation with ufoSighting as our generic iterator
-tableData.forEach(ufoSighting => {
-    // Create a new row for the table as a tr element
-    var row = tBody.append("tr")
-    // Loop through the object itself using forEach and key, value notation
-    Object.entries(ufoSighting).forEach(([key, value]) => {
-        // Create the cell in the table as a td element
-        var cell = row.append("td")
-        // grab the value from the object and insert it as the text of the cell
-        cell.text(value);
+// define our function to initiate our standard full table
+function init() {
+    // Loop through each object in the tableData array using arrow notation with ufoSighting as our generic iterator
+    tableData.forEach(ufoSighting => {
+        // Create a new row for the table as a tr element
+        var row = tBody.append("tr")
+        // Loop through the object itself using forEach and key, value notation
+        Object.entries(ufoSighting).forEach(([key, value]) => {
+            // Create the cell in the table as a td element
+            var cell = row.append("td")
+            // grab the value from the object and insert it as the text of the cell
+            cell.text(value);
+        });
+
     });
 
-});
+}
+
+// Write our filterTable function
+function filterTable() {
+    // first i need to clear out my existing table.
+    tBody.remove()
+    // Copying the above init function
+    tableData.forEach(ufoSighting => {
+        // Add conditional to check that the date matches the selected date
+        console.log(`Alien Date = ${alienDate.value}`)
+        console.log(`datetime = ${ufoSighting.datetime}`)
+        if (alienDate == ufoSighting.datetime) {
+            var row = tBody.append("tr")
+            // Loop through the object itself using forEach and key, value notation
+            Object.entries(ufoSighting).forEach(([key, value]) => {
+                // Create the cell in the table as a td element
+                var cell = row.append("td")
+                // grab the value from the object and insert it as the text of the cell
+                cell.text(value);
+            });
+        }
+    });
+}
+// create our event listener
+d3.selectAll("#searchButton").on("click", filterTable)
+
+init()
